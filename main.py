9822d467e201b3a0544678a549636a56fc7135f0
c-santana-sp <eduardomarquesdesantana@gmail.com>
2022-12-11 02:19:32 -0300

    dev: adding game base

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame, sys
+from settings import *
+# from debug import debug
+
+class Game:
+    def __init__(self) -> None:
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            self.screen.fill('black')
+
+            # debug('hello :)')
+
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
