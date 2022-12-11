74e417f490662de12162f56444bf10f222ba98aa
c-santana-sp <eduardomarquesdesantana@gmail.com>
2022-12-11 02:55:03 -0300

    feat: implementing level

---
+++ src/level.py
@@ -0,0 +1,28 @@
+import pygame
+from settings import *
+from tile import Tile
+from player import Player
+
+class Level:
+    def __init__(self):
+
+        self.display_surface = pygame.display.get_surface()
+
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacles_sprite = pygame.sprite.Group()
+
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for column_index, column in enumerate(row):
+                x = column_index * TILESIZE
+                y = row_index * TILESIZE
+
+                if column == 'x':
+                    Tile((x, y), [self.visible_sprites, self.obstacles_sprite])
+                elif column == 'p':
+                    Player((x, y), [self.visible_sprites])
+
+    def run(self):
+        self.visible_sprites.draw(self.display_surface)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame, sys
 from settings import *
 # from debug import debug
+from level import Level
 
 class Game:
     def __init__(self) -> None:
@@ -10,16 +11,20 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
         self.clock = pygame.time.Clock()
 
+        self.level = Level()
+
     def run(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            
+
             self.screen.fill('black')
 
             # debug('hello :)')
+
+            self.level.run()
 
             pygame.display.update()
             self.clock.tick(FPS)
