d51fec238434c377b13674ddd3b47f7ece66d614
c-santana-sp <eduardomarquesdesantana@gmail.com>
2022-12-11 03:13:43 -0300

    feat: implemeting simple player movement

--- src/level.py
+++ src/level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -22,7 +23,9 @@ class Level:
                 if column == 'x':
                     Tile((x, y), [self.visible_sprites, self.obstacles_sprite])
                 elif column == 'p':
-                    Player((x, y), [self.visible_sprites])
+                    self.player = Player((x, y), [self.visible_sprites])
 
     def run(self):
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
